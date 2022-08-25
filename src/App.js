dc56f4f60958674fb0b3ed392891c1265958f7c2
Harshit Srivastava <happylake161@gmail.com>
2022-08-25 07:27:46 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AiOutlineCheck } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="">
+      <div className="container-stack">
+        <div className="stack">
+          <div className="stack-left">
+            <div>
+              <p className="stack-para">Pricing</p>
+              <h2 className="stack-title">Get lifetime access</h2>
+            </div>
+            <p className="stack-para-2">
+              Choose from over 210+ beautiful and responsive components.
+            </p>
+          </div>
+          <div className="stack-right">
+            <div className="card">
+              <div className="card-pop">
+                <p>popular</p>
+              </div>
+              <div className="card-desc">
+                <div className="card-price-wrap">
+                  <div className="card-price">
+                    <h2 className="card-price-tag">$148</h2>
+                    <div className="card-detail">
+                      <p style={{ fontSize: "1.25rem", fontWeight: "600", margin: '0' }}>
+                        Application
+                      </p>
+                      <p style={{ marginTop: "0.25rem", color: "#4a5568" }}>
+                        For accessible applications
+                      </p>
+                    </div>
+                  </div>
+                  <ul className="card-list">
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Application components</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Unlimited projects</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Lifetime access</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Customer support</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Free updates</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <button className="card-btn">Get started</button>
+            </div>
+            <div className="card">
+              <div className="card-desc">
+                <div className="card-price-wrap">
+                  <div className="card-price">
+                    <h2 className="card-price-tag">$148</h2>
+                    <div className="card-detail">
+                      <p style={{ fontSize: "1.25rem", fontWeight: "600",  margin: '0' }}>
+                        Application
+                      </p>
+                      <p style={{ marginTop: "0.25rem", color: "#4a5568" }}>
+                        For accessible applications
+                      </p>
+                    </div>
+                  </div>
+                  <ul className="card-list">
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Application components</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Unlimited projects</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Lifetime access</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Customer support</p>
+                    </li>
+                    <li className="card-list-entry">
+                      <div className="card-tick"><AiOutlineCheck /></div>
+                      <p className="card-list-para">Free updates</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <button className="card-btn">Get started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
